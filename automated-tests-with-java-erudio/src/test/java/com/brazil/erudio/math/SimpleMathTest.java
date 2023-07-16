2e02b066c65f1577d6c4c5d9770e06b703a58cfb
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-16 13:14:15 -0300

    [desafio] implemente testes automatizados para as demais operacoes matematicas

--- automated-tests-with-java-erudio/src/test/java/com/brazil/erudio/math/SimpleMathTest.java
+++ automated-tests-with-java-erudio/src/test/java/com/brazil/erudio/math/SimpleMathTest.java
@@ -20,4 +20,63 @@ class SimpleMathTest {
         assertNotNull(actual);
     }
 
+    @Test
+    void testSubtraction() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+
+        Double actual = math.subtraction(firstNumber, secondNumber);
+        double expected = 4.2D;
+
+        assertEquals(expected, actual, () -> firstNumber + " + " + secondNumber + " did not produce "+ expected +"!");
+    }
+
+    @Test
+    void testMultiplication() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+
+        Double actual = math.multiplication(firstNumber, secondNumber);
+        double expected = 12.4D;
+
+        assertEquals(expected, actual, () -> firstNumber + " + " + secondNumber + " did not produce "+ expected +"!");
+    }
+
+    @Test
+    void testDivision() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+
+        Double actual = math.division(firstNumber, secondNumber);
+        double expected = 3.1D;
+
+        assertEquals(expected, actual, () -> firstNumber + " + " + secondNumber + " did not produce "+ expected +"!");
+    }
+
+    @Test
+    void testMean() {
+        SimpleMath math = new SimpleMath();
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+
+        Double actual = math.mean(firstNumber, secondNumber);
+        double expected = 4.1D;
+
+        assertEquals(expected, actual, () -> firstNumber + " + " + secondNumber + " did not produce "+ expected +"!");
+    }
+
+    @Test
+    void testSquareRoot() {
+        SimpleMath math = new SimpleMath();
+        double number = 16D;
+
+        Double actual = math.squareRoot(number);
+        double expected = 4D;
+
+        assertEquals(expected, actual, () -> number + " did not produce "+ expected +"!");
+    }
+
 }
