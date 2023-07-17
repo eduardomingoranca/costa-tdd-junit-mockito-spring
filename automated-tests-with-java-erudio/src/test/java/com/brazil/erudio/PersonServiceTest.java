1fcfa54667e380d59b484e3691e32833837faa37
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-17 20:26:42 -0300

    criando o teste de criacao de persons

---
+++ automated-tests-with-java-erudio/src/test/java/com/brazil/erudio/PersonServiceTest.java
@@ -0,0 +1,20 @@
+package com.brazil.erudio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PersonServiceTest {
+
+    // test[System Under Test]_[Condition or State Change]_[Expected Result]
+    @DisplayName("When Create a Person with Success Should Return a Person Object")
+    @Test
+    void testCreatePerson_WhenSuccess_ShouldReturnPersonObject() {
+        // Given / Arrange
+
+        // When / Act
+
+        // Then / Assert
+
+    }
+
+}
