a9ba2c62fa505085b8f1c240967bf5f9f64f95ea
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-17 20:53:18 -0300

    verificando se a pessoa criada contem um first name

--- automated-tests-with-java-erudio/src/main/java/com/brazil/erudio/service/PersonService.java
+++ automated-tests-with-java-erudio/src/main/java/com/brazil/erudio/service/PersonService.java
@@ -5,7 +5,7 @@ import com.brazil.erudio.model.Person;
 public class PersonService implements IPersonService {
     @Override
     public Person createPerson(Person person) {
-        return new Person();
+        return person;
     }
 
 }
--- automated-tests-with-java-erudio/src/test/java/com/brazil/erudio/PersonServiceTest.java
+++ automated-tests-with-java-erudio/src/test/java/com/brazil/erudio/PersonServiceTest.java
@@ -6,6 +6,7 @@ import com.brazil.erudio.service.PersonService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class PersonServiceTest {
@@ -30,6 +31,27 @@ class PersonServiceTest {
 
         // Then / Assert
         assertNotNull(actual, "The createPerson() should not have returned null!");
+    }
+
+    @DisplayName("When Create a Person with Success Should Contains FirstName in Returned Person Object")
+    @Test
+    void testCreatePerson_WhenSuccess_ShouldContainsFirstNameInReturnedPersonObject() {
+        // Given / Arrange
+        IPersonService service = new PersonService();
+
+        Person person = new Person(
+                "Keith",
+                "Moon",
+                "kmoon@erudio.com.br",
+                "Wembley - UK",
+                "Male"
+        );
+
+        // When / Act
+        Person actual = service.createPerson(person);
+
+        // Then / Assert
+        assertEquals(person.getFirstName(), actual.getFirstName(), () -> "The FirstName is Different!");
     }
 
 }
