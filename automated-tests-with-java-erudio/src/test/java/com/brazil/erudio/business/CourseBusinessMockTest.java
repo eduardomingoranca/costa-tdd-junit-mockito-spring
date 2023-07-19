4cf10a9cddf78b470e2a6e84e578d267aa3605bb
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-19 06:44:43 -0300

    setando os valores a serem retornados pelo mock

--- automated-tests-with-java-erudio/src/test/java/com/brazil/erudio/business/CourseBusinessMockTest.java
+++ automated-tests-with-java-erudio/src/test/java/com/brazil/erudio/business/CourseBusinessMockTest.java
@@ -4,25 +4,48 @@ import com.brazil.erudio.service.CourseService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CourseBusinessMockTest {
 
     CourseService mockService;
     CourseBusiness business;
+    List<String> courses;
 
     @BeforeEach
     void setup() {
         // Given / Arrange
         mockService = mock(CourseService.class);
         business = new CourseBusiness(mockService);
+        courses = asList(
+                "REST API's RESTFul do 0 a Azure com ASP.NET Core 5 e Docker",
+                "Agile Desmistificado com Scrum, XP, Kanban e Trello",
+                "Spotify Engineering Culture Desmistificado",
+                "REST API's RESTFul do 0 a AWS com Spring Boot 3 Java e Docker",
+                "Docker do Zero a Maestria - Conteinerizacao Desmistificada",
+                "Docker para Amazon AWS Implante Apps Java e .NET com Travis CI",
+                "Microsservicos do 0 com Spring Cloud, Spring Boot e Docker",
+                "Arquitetura de Microsservicos do 0 com ASP.NET, .NET 6 e C#",
+                "REST API's RESTFul do 0 a AWS com Spring Boot 3 Kotlin e Docker",
+                "Kotlin para DEV's Java: Aprenda a Linguagem Padrao do Android",
+                "Microsservicos do 0 com Spring Cloud, Kotlin e Docker"
+        );
     }
 
     @Test
     void testCoursesRelatedToSpring_When_UsingAMock() {
         // When / Act
+        when(mockService.retrieveCourses("Edward")).thenReturn(courses);
+
+        List<String> filteredCourses = business.retrieveCoursesRelatedToSpring("Edward");
 
         // Then / Assert
+        assertEquals(4, filteredCourses.size());
     }
 
 }
