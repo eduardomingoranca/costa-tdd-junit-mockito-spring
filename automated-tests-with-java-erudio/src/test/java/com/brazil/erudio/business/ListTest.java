7cc7a3da8be1219c56ddefe54b85bce069ae9f3c
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-19 18:56:03 -0300

    mockando listas

---
+++ automated-tests-with-java-erudio/src/test/java/com/brazil/erudio/business/ListTest.java
@@ -0,0 +1,61 @@
+package com.brazil.erudio.business;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ListTest {
+
+    @Test
+    void testMockingList_When_SizeIsCalled_ShouldReturn10() {
+        // Given / Arrange
+        List<?> list = mock(List.class);
+        when(list.size()).thenReturn(10);
+
+        // When / Act & Then / Assert
+        assertEquals(10, list.size());
+    }
+
+    @Test
+    void testMockingList_When_SizeIsCalled_ShouldReturnMultipleValues() {
+        // Given / Arrange
+        List<?> list = mock(List.class);
+        when(list.size()).thenReturn(10).thenReturn(20);
+
+        // When / Act & Then / Assert
+        assertEquals(10, list.size());
+        assertEquals(20, list.size());
+        assertEquals(20, list.size());
+    }
+
+    @Test
+    void testMockingList_When_GetIsCalled_ShouldReturnErudio() {
+        // Given / Arrange
+        List<Object> list = mock(List.class);
+        when(list.get(0)).thenReturn("Erudio");
+
+        // When / Act & Then / Assert
+        assertEquals("Erudio", list.get(0));
+        assertNull(list.get(1));
+    }
+
+    @Test
+    void testMockingList_When_GetIsCalledWithArgumentMatcher_ShouldReturnErudio() {
+        // Given / Arrange
+        List<Object> list = mock(List.class);
+
+        // If you are using argument matchers, all arguments
+        // have to be provided by matchers.
+        when(list.get(anyInt())).thenReturn("Erudio");
+
+        // When / Act & Then / Assert
+        assertEquals("Erudio", list.get(anyInt()));
+    }
+
+}
