53708eb175146cc9b3723973bdf86e1ff94e60a6
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-19 05:48:13 -0300

    entendendo o conceito de system (method) under test

---
+++ automated-tests-with-java-erudio/src/main/java/com/brazil/erudio/CourseBusiness.java
@@ -0,0 +1,32 @@
+package com.brazil.erudio;
+
+import com.brazil.erudio.service.CourseService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// CourseBusiness = SUT - System (Method) Under Test
+public class CourseBusiness {
+
+    // CourseService is a Dependency
+    private final CourseService service;
+
+    public CourseBusiness(CourseService service) {
+        this.service = service;
+    }
+
+    public List<String> retrieveCoursesRelatedToSpring(String student) {
+        ArrayList<String> filteredCourses = new ArrayList<>();
+
+        // buscando todos os cursos que o estudante esta inscrito
+        List<String> allCourses = service.retrieveCourses(student);
+
+        // filtrando o curso Spring
+        for (String course: allCourses) {
+            if (course.contains("Spring"))
+                filteredCourses.add(course);
+        }
+
+        return filteredCourses;
+    }
+}
---
+++ automated-tests-with-java-erudio/src/main/java/com/brazil/erudio/service/CourseService.java
@@ -0,0 +1,8 @@
+package com.brazil.erudio.service;
+
+import java.util.List;
+
+public interface CourseService {
+    List<String> retrieveCourses(String student);
+
+}
