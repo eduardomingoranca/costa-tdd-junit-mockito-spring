6839d2222d35cfffdb00cca0cc61d9fb38900e38
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-22 15:20:12 -0300

    entendendo as annotations rest controller, request mapping e path variable

---
+++ erudio/src/main/java/com/brazil/erudio/controllers/MathController.java
@@ -0,0 +1,18 @@
+package com.brazil.erudio.controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class MathController {
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = GET)
+    public Double sum(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) {
+        return 1D;
+    }
+
+}
