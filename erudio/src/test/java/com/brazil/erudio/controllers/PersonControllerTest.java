1dfefabfdc575486120f1c4c2c68e5691afcbb0e
eduardomingoranca <eduardomingoranca@gmail.com>
2023-07-29 16:57:34 -0300

    testando a operacao de findall

---
+++ erudio/src/test/java/com/brazil/erudio/controllers/PersonControllerTest.java
@@ -0,0 +1,65 @@
+package com.brazil.erudio.controllers;
+
+import com.brazil.erudio.models.Person;
+import com.brazil.erudio.services.PersonService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
+
+@WebMvcTest
+class PersonControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @MockBean
+    private PersonService service;
+
+    private Person person;
+
+    @BeforeEach
+    void setup() {
+        // Given / Arrange
+        person = new Person("Moses", "Cohen",
+                "Mayfair - London - UK", "Male",
+                "moses@erudio.com");
+    }
+
+    @DisplayName("Given Person Object When Create Person then Return Saved Person")
+    @Test
+    void testGivenPersonObject_WhenCreatePerson_thenReturnSavedPerson() throws Exception {
+        // Given / Arrange
+        given(service.create(any(Person.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        // When / Act
+        ResultActions response = mockMvc.perform(post("/person")
+                .contentType(APPLICATION_JSON)
+                .content(mapper.writeValueAsString(person)));
+
+        // Then / Assert
+        response.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName", is(person.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(person.getLastName())))
+                .andExpect(jsonPath("$.email", is(person.getEmail())));
+    }
+
+}
